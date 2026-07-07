bfbef0b5c418ffde3e0b99240a9a91f5e63f3717
sssmpca627 <sumah.shutcho.shuri@gmail.com>
2026-07-07 16:02:59 +0900

    Update prices.js

--- prices.js
+++ prices.js
@@ -78,30 +78,37 @@ const prices = {
 
 const models = [
 
-"iPhone 12",
-"iPhone 12 mini",
-"iPhone 12 Pro",
-"iPhone 12 Pro Max",
-
-"iPhone 13",
-"iPhone 13 mini",
-"iPhone 13 Pro",
-"iPhone 13 Pro Max",
-
-"iPhone 14",
-"iPhone 14 Plus",
-"iPhone 14 Pro",
-"iPhone 14 Pro Max",
-
-"iPhone 15",
-"iPhone 15 Plus",
-"iPhone 15 Pro",
-"iPhone 15 Pro Max",
-
-"iPhone 16",
-"iPhone 16 Plus",
+"iPhone 16 Pro Max",
 "iPhone 16 Pro",
-"iPhone 16 Pro Max"
+"iPhone 16 Plus",
+"iPhone 16",
+
+"iPhone 15 Pro Max",
+"iPhone 15 Pro",
+"iPhone 15 Plus",
+"iPhone 15",
+
+"iPhone 14 Pro Max",
+"iPhone 14 Pro",
+"iPhone 14 Plus",
+"iPhone 14",
+
+"iPhone 13 Pro Max",
+"iPhone 13 Pro",
+"iPhone 13 mini",
+"iPhone 13",
+
+"iPhone 12 Pro Max",
+"iPhone 12 Pro",
+"iPhone 12 mini",
+"iPhone 12",
+
+"iPhone 11 Pro Max",
+"iPhone 11 Pro",
+"iPhone 11",
+
+"iPhone SE（第3世代）",
+"iPhone SE（第2世代）"
 
 ];
 
